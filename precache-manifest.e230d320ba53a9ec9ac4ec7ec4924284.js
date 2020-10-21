0b17432ad42fe00d7be4b44f423a29ad2a457861
Bignichok <bignik545@gmail.com>
2020-10-21 18:18:33 +0300

    Updates

---
+++ precache-manifest.e230d320ba53a9ec9ac4ec7ec4924284.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcc93659cf3654b0e154371f338f07e",
+    "url": "/react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "833c9f7df6f921c962d5",
+    "url": "/react-hw-05-phonebook/static/css/main.5bd6d07d.chunk.css"
+  },
+  {
+    "revision": "5b216da389a8f9af0465",
+    "url": "/react-hw-05-phonebook/static/js/2.a061b97c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-hw-05-phonebook/static/js/2.a061b97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833c9f7df6f921c962d5",
+    "url": "/react-hw-05-phonebook/static/js/main.ba08a408.chunk.js"
+  },
+  {
+    "revision": "64a39d755718f1ef423c",
+    "url": "/react-hw-05-phonebook/static/js/runtime-main.a988175b.js"
+  },
+  {
+    "revision": "7f17a4d2c8808510ba61bf93818b90cb",
+    "url": "/react-hw-05-phonebook/static/media/close.7f17a4d2.svg"
+  }
+]);
